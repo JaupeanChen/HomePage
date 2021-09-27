6e75eb055ed4bc2c96b8a7105fe9e834cfe44166
Jarvan <1129561405@qq.com>
2021-09-27 16:46:30 +0800

    引入购物车服务暴露组件

--- module_homepage/src/main/java/com/me/module_homepage/HomePageActivity.java
+++ module_homepage/src/main/java/com/me/module_homepage/HomePageActivity.java
@@ -1,9 +1,11 @@
 package com.me.module_homepage;
 
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.me.export_cart.CartServiceUtil;
 import com.me.module_common.BaseActivity;
 
 @Route(path = "/homepage/HomePageActivity")
@@ -23,10 +25,13 @@ public class HomePageActivity extends BaseActivity {
     public void initData() {
         Button button = findViewById(R.id.btn_go);
         button.setOnClickListener(v ->
-                ARouter.getInstance()
-                        .build("/cart/CartActivity")
-                        .navigation()
+//                ARouter.getInstance()
+//                        .build("/cart/CartActivity")
+//                        .navigation()
+                        CartServiceUtil.navigateToCart("param1", "param2")
         );
-
+        TextView tvCount = findViewById(R.id.tv_count);
+        String count = "购物车商品数量：" + CartServiceUtil.getCartInfo().getCount();
+        tvCount.setText(count);
     }
 }
